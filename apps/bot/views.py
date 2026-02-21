fc3ce0c711de132d18541ca0440b28289a3023a6
Tulqin Urinov <tulqinurinov005@gmail.com>
2026-02-21 21:08:26 +0500

    a

--- apps/bot/views.py
+++ apps/bot/views.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-from django.conf import settings
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
